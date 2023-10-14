a3c2ce00c1aa33404d8d7806adeb75341bf687f3
Rrens <yusufrendi313@gmail.com>
2023-10-14 11:28:18 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('phone_number');
+            $table->longText('address');
+            $table->string('city');
+            $table->string('province');
+            $table->string('profile_picture');
+            // $table->string('token');
+            // $table->string('refresh_token');
+            $table->dateTime('last_login');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
